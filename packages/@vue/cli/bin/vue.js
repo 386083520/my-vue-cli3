d69ce5ca3bda132ef7851d655391babd20a405f4
386083520 <386083520@qq.com>
2021-11-07 22:12:51 +0800

    引入create命令

--- packages/@vue/cli/bin/vue.js
+++ packages/@vue/cli/bin/vue.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
-console.log('vue3')
+const program = require('commander')
+program
+    .command('create <app-name>')
+    .description('create a new project powered by vue-cli-service')
+    .action((name, cmd) => {
+        const options = cleanArgs(cmd)
+        require('../lib/create')(name, options)
+    })
+program.parse(process.argv)
+
+function cleanArgs (cmd) {
+    return cmd
+}
---
+++ packages/@vue/cli/lib/create.js
@@ -0,0 +1,5 @@
+function create (projectName, options) {
+    console.log('create111')
+}
+module.exports = create
+
