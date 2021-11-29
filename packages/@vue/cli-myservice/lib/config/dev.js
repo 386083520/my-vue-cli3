eef19915e8314c973017f1bef681fb0a367f3e46
386083520 <386083520@qq.com>
2021-11-29 08:11:35 +0800

    dev里面的chainWebpack配置

--- packages/@vue/cli-myservice/lib/config/dev.js
+++ packages/@vue/cli-myservice/lib/config/dev.js
@@ -1,3 +1,22 @@
 module.exports = (api, options) => {
-    api.chainWebpack(webpackConfig => {})
+    api.chainWebpack(webpackConfig => {
+        if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
+            webpackConfig
+                .devtool('cheap-module-eval-source-map') // 代码映射
+                .output
+                .publicPath(options.publicPath)
+            webpackConfig
+                .plugin('hmr')
+                .use(require('webpack/lib/HotModuleReplacementPlugin')) // 启用热替换
+            webpackConfig
+                .output
+                .globalObject(`(typeof self !== 'undefined' ? self : this)`)
+
+            if (!process.env.VUE_CLI_TEST && options.devServer.progress !== false) {
+                webpackConfig
+                    .plugin('progress')
+                    .use(require('webpack/lib/ProgressPlugin'))
+            }
+        }
+    })
 }
