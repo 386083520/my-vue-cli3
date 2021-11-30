112ca227d072d36f3c5502d2878b213d103142fd
386083520 <386083520@qq.com>
2021-11-30 08:30:30 +0800

    base里面的配置说明（6）

--- packages/@vue/cli-myservice/lib/config/base.js
+++ packages/@vue/cli-myservice/lib/config/base.js
@@ -143,7 +143,25 @@ module.exports = (api, options) => {
             .end()
             .end()
 
+        webpackConfig.node
+            .merge({
+                // prevent webpack from injecting useless setImmediate polyfill because Vue
+                // source contains it (although only uses it if it's native).
+                setImmediate: false,
+                // prevent webpack from injecting mocks to Node native modules
+                // that does not make sense for the client
+                dgram: 'empty',
+                fs: 'empty',
+                net: 'empty',
+                tls: 'empty',
+                child_process: 'empty'
+            })
 
-
+        const resolveClientEnv = require('../util/resolveClientEnv')
+        webpackConfig
+            .plugin('define')
+            .use(require('webpack/lib/DefinePlugin'), [
+                resolveClientEnv(options)
+            ])
     })
 }
